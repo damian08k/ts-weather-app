a98198f8f01af41583aff91afa6256314a03a386
damian08k <damiankierzkowski08@wp.pl>
2021-06-13 22:35:15 +0200

    Fix: ForkTsChecker Plugin has wrong eslint config

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,69 @@
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const path = require("path");
+const path = require('path');
 
 const webpackConfig = {
-    mode: "development",
-    entry: [__dirname + "/src/index.ts", __dirname + "/src/style.scss"],
+    mode: 'development',
+    entry: [__dirname + '/src/index.ts', __dirname + '/src/style.scss'],
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
-    target: "es5",
+    target: 'es5',
     devServer: {
-        contentBase: path.join(__dirname, "dist"),
+        contentBase: path.join(__dirname, 'dist'),
         port: 3300,
     },
     module: {
         rules: [
             {
                 test: /\.ts?$/,
-                use: "ts-loader",
-                include: [path.resolve(__dirname, "src")],
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')],
             },
             {
                 test: /\.s[ac]ss$/i,
                 use: [
                     {
-                        loader: "file-loader",
+                        loader: 'file-loader',
                         options: {
-                            outputhPath: "dist/",
-                            name: "style.css",
+                            outputhPath: 'dist/',
+                            name: 'style.css',
                         },
                     },
-                    "sass-loader",
+                    'sass-loader',
                 ],
             },
         ],
     },
     plugins: [
         new CopyPlugin({
-            patterns: [{
-                from: "src/assets",
-                to: "assets",
-            }]
+            patterns: [
+                {
+                    from: 'src/assets',
+                    to: 'assets',
+                },
+            ],
         }),
         new ESLintPlugin({
-            extensions: [".ts", ".js"],
-            exclude: "node_modules",
+            extensions: ['.ts', '.js'],
+            exclude: 'node_modules',
         }),
         new ForkTsCheckerWebpackPlugin({
-            eslint: true,
+            eslint: {
+                files: './src/*.ts',
+            },
         }),
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
+            template: './src/index.html',
+        }),
     ],
     resolve: {
-        extensions: [".ts", ".js"],
+        extensions: ['.ts', '.js'],
     },
 };
 
